483faded78d35570488d3b8d7e32dac51606c012
TuncayBasak25 <tuncay.basak@outlook.com>
2020-10-05 16:59:07 +0200

    S

--- models/index.js
+++ models/index.js
@@ -14,8 +14,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
+    port:     5432,
+    host:     'dla3bedrnphjg',
     logging:  true //false
   })
 } else {
