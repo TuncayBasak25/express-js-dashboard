dfc5e03977c7fd52ee3bdc3be14c10c26fbf8810
Tuncay Basak <tuncay.basak@outlook.com>
2020-10-06 21:11:55 +0200

    S

---
+++ routes/fetch.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../models/index');
+
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  try
+  {
+    const books = db.Book.findAll();
+
+    res.send(books);
+  }
+  catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('')
+
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../models/index');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+
 
 module.exports = router;
